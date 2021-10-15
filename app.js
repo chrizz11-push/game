6803f295bd3a4c0fe71e749f729a40db1c387089
chrizz11-push <christopherchima11@gmail.com>
2021-10-15 17:11:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose") 
+const port = 2000
+const local = "mongodb://localhost/Game"
+const app = express()
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(process.env.mongod, {
+    useNewUrlParser:true,
+})
+.then(() => {
+    console.log("server is connected to mongoose")
+})
+.catch((err) => {
+    console.log(err.message)
+})
+app.get("/", (req,res) => {
+    res.send("server is connected to mongodb")
+})
+app.listen(port, () => {
+    console.log(`server is listening to:${port}`)
+})
